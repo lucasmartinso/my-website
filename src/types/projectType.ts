24fb77f505e1f18e5ba4a455f7d4a611c8040c79
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-14 23:29:44 -0300

    feat: create type for project and enum for type

---
+++ src/types/projectType.ts
@@ -0,0 +1,16 @@
+export interface projectInfo { 
+    name: string;
+    type: types;
+    image: string; 
+    description: string; 
+    url: string | null; 
+    documentation: string; 
+    front: string | null; 
+    back: string | null; 
+    pinned: boolean;
+} 
+
+enum types { 
+    web = "web", 
+    notebook = "notebook"
+}
