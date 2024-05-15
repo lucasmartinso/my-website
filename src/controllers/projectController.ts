5dfbc652c8725ec84ae89b8c6c6c58aa2b26335e
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-15 14:49:28 -0300

    feat: create all routes of the project

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as projectService from "../services/projectService";
+import { projectInfo } from "../types/projectType";
 
 export async function addProject(req: Request, res: Response): Promise<void> {
     
@@ -14,7 +15,14 @@ export async function updateProjet(req: Request, res: Response): Promise<void> {
 } 
 
 export async function getProjects(req: Request, res: Response): Promise<void> { 
-    const projects: any = await projectService.getProjects(); 
+    const projects: projectInfo[] = await projectService.getProjects(); 
 
+    res.status(200).send(projects);
+}  
+
+export async function getProjectsType(req: Request, res: Response): Promise<void> {
+    const type: string = req.params.type;
+    const projects: projectInfo[] = await projectService.getProjects(); 
+    
     res.status(200).send(projects);
 }
--- src/routers/projectRouter.ts
+++ src/routers/projectRouter.ts
@@ -4,5 +4,9 @@ import * as projectController from "../controllers/projectController";
 const projectRouter = Router(); 
 
 projectRouter.get("/projects", projectController.getProjects);
+projectRouter.get("/projects/:type", projectController.getProjectsType);
+projectRouter.post("/projects", projectController.addProject);
+projectRouter.put("/edit/projects/:name", projectController.updateProjet)
+projectRouter.delete("/delete/projects/:name", projectController.excludeProject)
 
 export default projectRouter;
