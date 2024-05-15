45485b2a93ab0fff15abd33fe1040e07b3064b0e
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-15 12:14:02 -0300

    feat: create service for get all projects

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as projectRepositorie from "../repositories/projectRepositorie";
 
 export async function addProject(req: Request, res: Response): Promise<void> {
     
@@ -13,5 +14,7 @@ export async function updateProjet(req: Request, res: Response): Promise<void> {
 } 
 
 export async function getProjects(req: Request, res: Response): Promise<void> { 
-    
+    const projects = await projectRepositorie.getProjects();
+
+    return projects;
 }
