f4ac9f48dd994e34d55b8fbdf0a168fc8cba5fd4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-15 12:22:23 -0300

    feat: create controller for get all projects

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express";
+import * as projectService from "../services/projectService";
 
-export async function addProject(req: Request, res: Response) {
+export async function addProject(req: Request, res: Response): Promise<void> {
     
 } 
 
-export async function excludeProject(req: Request, res: Response) {
+export async function excludeProject(req: Request, res: Response): Promise<void> {
     
 } 
 
-export async function updateProjet(req: Request, res: Response) {
+export async function updateProjet(req: Request, res: Response): Promise<void> {
     
 } 
 
-export async function getProjects(req: Request, res: Response) { 
-    
+export async function getProjects(req: Request, res: Response): Promise<void> { 
+    const projects = await projectService.getProjects(); 
+
+    res.status(200).send(projects);
 }
--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import * as projectRepositorie from "../repositories/projectRepositorie";
 
-export async function addProject(req: Request, res: Response): Promise<void> {
+export async function addProject() {
     
 } 
 
-export async function excludeProject(req: Request, res: Response): Promise<void> {
+export async function excludeProject() {
     
 } 
 
-export async function updateProjet(req: Request, res: Response): Promise<void> {
+export async function updateProjet() {
     
 } 
 
-export async function getProjects(req: Request, res: Response): Promise<void> { 
+export async function getProjects() { 
     const projects = await projectRepositorie.getProjects();
 
     return projects;
