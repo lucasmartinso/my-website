edc7411f5503ae46c7f5c52a0b6c508c5e79cedb
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-16 15:16:42 -0300

    fix: create regex pattern for project schema

--- src/routers/projectRouter.ts
+++ src/routers/projectRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import * as projectController from "../controllers/projectController";
+import schemaValidator from "../middlewares/schemaValidator";
+import { projectSchema } from "../schemas/projectSchema";
 
 const projectRouter = Router(); 
 
 projectRouter.get("/projects", projectController.getProjects);
 projectRouter.get("projects/pinned", projectController.getPinnedProjects);
 projectRouter.get("/projects/:id", projectController.getProjectInfo);
-projectRouter.post("/projects", projectController.addProject);
+projectRouter.post("/projects", schemaValidator(projectSchema), projectController.addProject);
 projectRouter.put("/edit/projects/:id", projectController.updateProjet)
 projectRouter.delete("/delete/projects/:id", projectController.deleteProject)
 
