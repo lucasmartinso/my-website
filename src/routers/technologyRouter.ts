dbc5de55db83f5b5fcbc7672067f2ffe9feda90d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-17 15:47:32 -0300

    feat: create technology router

---
+++ src/routers/technologyRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as technologyController from "../controllers/technologyController";
+import schemaValidator from "../middlewares/schemaValidator";
+import { technologySchema } from "../schemas/technologySchema";
+
+const projectRouter = Router(); 
+
+projectRouter.get("/techs", technologyController.getTechnologies);
+projectRouter.post("/techs", schemaValidator(technologySchema), technologyController.addTechnology);
+projectRouter.delete("/delete/techs/:id", technologyController.deleteTechnology);
+projectRouter.put("/edit/techs/:id", schemaValidator(technologySchema), technologyController.updateTechnology);
+
+export default projectRouter;
