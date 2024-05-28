b2fe02be46beb4f581df307d0b612088a6dbd1ad
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-28 11:11:59 -0300

    feat: create route to send emails to me

---
+++ src/routers/personalRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as projectController from "../controllers/projectController";
+import schemaValidator from "../middlewares/schemaValidator";
+import { projectSchema } from "../schemas/projectSchema";
+
+const personalRouter = Router(); 
+
+personalRouter.post("/email",);
+
+
+export default personalRouter;
