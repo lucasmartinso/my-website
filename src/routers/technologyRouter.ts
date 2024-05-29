da69ea817d45f7bb128b42d8022f20acd25111cf
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-28 23:25:02 -0300

    feat: create route to tech search

--- src/routers/technologyRouter.ts
+++ src/routers/technologyRouter.ts
@@ -7,6 +7,7 @@ const projectRouter = Router();
 
 projectRouter.get("/techs", technologyController.getTechnologies);
 projectRouter.post("/techs", schemaValidator(technologySchema), technologyController.addTechnology);
+projectRouter.post("/techs/especify", technologyController.searchTech);
 projectRouter.delete("/delete/techs/:id", technologyController.deleteTechnology);
 projectRouter.put("/edit/techs/:id", schemaValidator(technologySchema), technologyController.updateTechnology);
 
