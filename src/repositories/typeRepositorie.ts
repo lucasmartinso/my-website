d9ebc81cc3b937ede95662f6bfdded6e93bf17cf
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 14:40:35 -0300

    feat: create repositorie post type

--- src/repositories/typeRepositorie.ts
+++ src/repositories/typeRepositorie.ts
@@ -1,10 +1,27 @@
 import { QueryResult } from "pg";
 import connection from "../databases/postgres";
+import { ptype } from "../types/typeType";
 
-export async function getTypes() {
-    const { rows: types } = await connection.query(`
-        SELECT * FROM type
-    `) 
+export async function getTypes(): Promise<ptype[]> {
+    const { rows: types }: QueryResult<ptype> = await connection.query(`
+        SELECT * FROM "type"
+    `); 
 
     return types;
+}
+
+export async function existNameType(type: string): Promise<ptype[]> {
+    const { rows: existName }: QueryResult<ptype> = await connection.query(`
+        SELECT * FROM "type"
+        WHERE name = $1
+    `,[type]); 
+
+    return existName;
+}
+
+export async function postType(name: string): Promise<void> {
+    await connection.query(`
+        INSERT INTO "type" (name) 
+        VALUES ($1)
+    `,[name]); 
 }
