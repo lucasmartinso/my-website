986b8ccca81e5abe9d536c366d5e46ac3c7c9522
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 15:25:13 -0300

    fix: update error to not found type id

--- src/services/typeService.ts
+++ src/services/typeService.ts
@@ -18,15 +18,17 @@ export async function postTypes(name: string): Promise<void> {
 export async function deleteType(id: number): Promise<void> {
     const existType: ptype[] = await typeRepository.existType(id);
 
-    if(existType) throw { type: "Conflit", message: "Tipo não existente"}
+    if(existType) throw { type: "Not Found", message: "Tipo não existente"}
 
     await typeRepository.deleteType(id);
 }
 
 export async function updateType(id: number, name: string): Promise<void> {
     const existType: ptype[] = await typeRepository.existType(id);
+    if(existType) throw { type: "Not Found", message: "Tipo não existente"}
 
-    if(existType) throw { type: "Conflit", message: "Tipo não existente"}
+    const existName: ptype[] = await typeRepository.existNameType(name);
+    if(existName) throw { type: "Conflit", message: "Nome já existente"}
 
     await typeRepository.updateType(id,name);
 }
