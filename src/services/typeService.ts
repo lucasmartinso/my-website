d159d83f1924b09b919c466d60bde86c8e1d141d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 14:50:13 -0300

    feat: create service get type

--- src/services/typeService.ts
+++ src/services/typeService.ts
@@ -1,1 +1,8 @@
-import * as typeRepository from "../repositories/typeRepositorie"
+import * as typeRepository from "../repositories/typeRepository"; 
+import { ptype } from "../types/typeType";
+
+export async function getTypes() {
+    const types: ptype[] = await typeRepository.getTypes();
+
+    return types;
+}
