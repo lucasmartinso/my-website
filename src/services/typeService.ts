a5431e0f58cddf2d6ad822cea530a512dc7fcd0a
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 14:55:55 -0300

    feat: create service post type

--- src/services/typeService.ts
+++ src/services/typeService.ts
@@ -1,8 +1,16 @@
 import * as typeRepository from "../repositories/typeRepository"; 
 import { ptype } from "../types/typeType";
 
-export async function getTypes() {
+export async function getTypes(): Promise<ptype[]> {
     const types: ptype[] = await typeRepository.getTypes();
 
     return types;
+}
+
+export async function postTypes(name: string): Promise<void> {
+    const existName: ptype[] = await typeRepository.existNameType(name);
+
+    if(existName) throw { type: "Conflit", message: "Nome já existente"}
+
+    await typeRepository.postType(name);
 }
