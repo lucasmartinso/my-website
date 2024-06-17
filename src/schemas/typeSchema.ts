663e0d22cc36bb850063add3fcf83b4df2589ed7
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 14:32:53 -0300

    feat: create types schema

--- src/schemas/typeSchema.ts
+++ src/schemas/typeSchema.ts
@@ -1,5 +1,5 @@
 import joi from "joi";
 
-export const technologySchema = joi.object({ 
+export const typeSchema = joi.object({ 
     name: joi.string().min(1).max(50).required().label("Nome tem que ter entre 1 e 30 caracteres")
 });
