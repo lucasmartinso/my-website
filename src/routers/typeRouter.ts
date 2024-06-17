afb26d40b1d810effb060c92c3449da62d0dc18a
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-17 15:41:05 -0300

    fix: router types name

--- src/routers/typeRouter.ts
+++ src/routers/typeRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import * as typeController from "../controllers/typeController";
 import schemaValidator from "../middlewares/schemaValidator";
-import { technologySchema } from "../schemas/technologySchema";
+import { typeSchema } from "../schemas/typeSchema";
 import { validateTokenAuth } from "../middlewares/authMiddleware";
 
-const projectRouter = Router(); 
+const typeRouter = Router(); 
 
-projectRouter.get("/types", typeController.getTypes);
-projectRouter.post("/types", schemaValidator(technologySchema), validateTokenAuth, typeController.postTypes);
-projectRouter.delete("/delete/types/:id", validateTokenAuth, typeController.deleteTypes);
-projectRouter.put("/edit/types/:id", schemaValidator(technologySchema), validateTokenAuth, typeController.updateTypes);
+typeRouter.get("/types", typeController.getTypes);
+typeRouter.post("/types", schemaValidator(typeSchema), validateTokenAuth, typeController.postTypes);
+typeRouter.delete("/delete/types/:id", validateTokenAuth, typeController.deleteTypes);
+typeRouter.put("/edit/types/:id", schemaValidator(typeSchema), validateTokenAuth, typeController.updateTypes);
 
-export default projectRouter;
+export default typeRouter;
