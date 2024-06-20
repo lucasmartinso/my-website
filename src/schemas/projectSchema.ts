85cd4b3738e827a3451c1f5d5df1305a9710a68d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-06-20 10:41:23 -0300

    fix: schema project

--- src/schemas/projectSchema.ts
+++ src/schemas/projectSchema.ts
@@ -3,7 +3,7 @@ import joi from "joi";
 export const projectSchema = joi.object({ 
     name: joi.string().min(2).max(30).required().label("Nome tem que ter entre 2 e 30 caracteres"),
     type: joi.string().min(2).max(30).required().label("Typo tem que conter entre 2 e 30 caracteres"),
-    image: joi.string().uri().required().label("Imagem tem que ser uma url"),
+    image: joi.string().pattern(/^https:\/\//).required().label("Imagem tem que ser uma url"),
     description: joi.string().min(15).max(500).required().label("Descrição obrigatoria com no minimo 15 caracteres e no maximo 500"),
     url: joi.string().uri().allow(null).label("Url do deploy tem que ser de uma url"),
     documentation: joi.string().uri().pattern(/^https:\/\/drive\.google\.com\/file\//).required().label("Documentação tem que ser uma url do drive, ex: https://drive.google.com/file/ ..."), 
