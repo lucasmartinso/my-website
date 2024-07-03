d1e2e99e1efe0154a27e6b3d4e2a81d94727e066
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-03 18:12:00 -0300

    feat: create blog routers

---
+++ src/routers/blogRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import schemaValidator from "../middlewares/schemaValidator";
+import { validateTokenAuth } from "../middlewares/authMiddleware";
+
+const blogRouter = Router(); 
+
+blogRouter.get("/blogs");
+blogRouter.get("/blogs/random");
+blogRouter.post("/blogs");
+blogRouter.put("/edit/blogs/:id");
+blogRouter.delete("/delete/blogs/:id");
+
+export default blogRouter;
