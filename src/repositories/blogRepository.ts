cd25979b890fc915c73bab925e378f22886fd9c2
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-03 18:30:04 -0300

    feat: create post blog repo

--- src/repositories/blogRepository.ts
+++ src/repositories/blogRepository.ts
@@ -1,15 +1,21 @@
+import { QueryResult } from "pg";
 import connection from "../databases/postgres";
+import { blogInfo } from "../types/blogType";
 
-export async function getBlogs(): Promise<mkcdmkcd> {
-    const { rows: blogs } = await connection.query(`
+export async function getBlogs(): Promise<blogInfo[]> {
+    const { rows: blogs }: QueryResult<blogInfo> = await connection.query(`
         SELECT * FROM "blog"    
     `)
 
     return blogs;
 }
 
-export async function postBlog(params:type) {
-    
+export async function postBlog(blogData: Omit<blogInfo, 'id'>): Promise<void> {
+    await connection.query(` 
+        INSERT INTO "blog" 
+        (tittle, description, text)  
+        VALUES ($1,$2,$3)  
+    `,[blogData.tittle,blogData.description,blogData.tittle]);
 }
 
 export async function updateBlog(params:type) {
