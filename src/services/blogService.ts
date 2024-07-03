cc8387ceefc1eeca9449229b49cef027da795688
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-03 18:56:22 -0300

    feat: create blog service

---
+++ src/services/blogService.ts
@@ -0,0 +1,19 @@
+export async function getBlogs() {
+    
+}
+
+export async function getRandomBlogs() {
+    
+}
+
+export async function postBlog() {
+    
+}
+
+export async function updateBlog() {
+    
+}
+
+export async function deleteBlog() {
+    
+}
