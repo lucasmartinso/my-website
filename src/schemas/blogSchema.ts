4f7c71e79a8b577c5188f3a907f518f8e30344a0
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-04 15:10:09 -0300

    feat: create text schema

---
+++ src/schemas/blogSchema.ts
@@ -0,0 +1,7 @@
+import joi from "joi";
+
+export const projectSchema = joi.object({ 
+    tittle: joi.string().min(2).required().label("Nome tem que ter no mínimo 2 caracteres"),
+    description: joi.string().min(10).required().label("Descrição obrigatoria com no minimo 10 caracteres"),
+    text: joi.string().min(50).required().label("Texto obrigatorio com no minimo 50 caracteres")
+});
