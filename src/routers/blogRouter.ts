0988c38cfcd7718d41eed4057a6158b726aefd77
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-04 15:06:50 -0300

    feat: complete route with controllers

--- src/routers/blogRouter.ts
+++ src/routers/blogRouter.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator";
 import { validateTokenAuth } from "../middlewares/authMiddleware";
+import * as blogController from "../controllers/blogController";
 
 const blogRouter = Router(); 
 
-blogRouter.get("/blogs");
-blogRouter.get("/blogs/random");
-blogRouter.get("/blogs/:id");
-blogRouter.post("/blogs");
-blogRouter.put("/edit/blogs/:id");
-blogRouter.delete("/delete/blogs/:id");
+blogRouter.get("/blogs", blogController.getBlogs);
+blogRouter.get("/blogs/random", blogController.getRandomBlogs);
+blogRouter.get("/blogs/:id", blogController.getCompleteBlog);
+blogRouter.post("/blogs", validateTokenAuth, blogController.postBlog);
+blogRouter.put("/edit/blogs/:id", validateTokenAuth, blogController.postBlog);
+blogRouter.delete("/delete/blogs/:id", validateTokenAuth, blogController.postBlog);
 
 export default blogRouter;
