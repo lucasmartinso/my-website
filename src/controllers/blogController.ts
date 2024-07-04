b1d5149e0be864c969ab5a8ea10721a72f0764fa
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-04 14:51:38 -0300

    feat: create get blogs controller

--- src/controllers/blogController.ts
+++ src/controllers/blogController.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
+import { blogInfo } from "../types/blogType";
+import * as blogService from "../services/blogService";
 
-export async function (params:type) {
+export async function getBlogs(req: Request, res: Response): Promise<void> {
+    const blogs: blogInfo[] = await blogService.getBlogs(); 
+
+    res.status(200).send(blogs);
+}
+
+export async function getRandomBlogs(req: Request, res: Response): Promise<void> {
+    
+}
+
+export async function postBlog(req: Request, res: Response): Promise<void> {
+    
+}
+
+export async function updateBlog(req: Request, res: Response): Promise<void> {
+    
+}
+
+export async function deleteBlog(req: Request, res: Response): Promise<void> {
     
 }
