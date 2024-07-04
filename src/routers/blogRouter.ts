6ad8b47ee7ea06697cbaece41ef2515efcfd5cc4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-07-04 15:13:03 -0300

    fix: schema validator for blog

--- src/routers/blogRouter.ts
+++ src/routers/blogRouter.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator";
 import { validateTokenAuth } from "../middlewares/authMiddleware";
 import * as blogController from "../controllers/blogController";
+import { blogSchema } from "../schemas/blogSchema";
 
 const blogRouter = Router(); 
 
 blogRouter.get("/blogs", blogController.getBlogs);
 blogRouter.get("/blogs/random", blogController.getRandomBlogs);
 blogRouter.get("/blogs/:id", blogController.getCompleteBlog);
-blogRouter.post("/blogs", validateTokenAuth, blogController.postBlog);
-blogRouter.put("/edit/blogs/:id", validateTokenAuth, blogController.postBlog);
+blogRouter.post("/blogs", validateTokenAuth, schemaValidator(blogSchema),blogController.postBlog);
+blogRouter.put("/edit/blogs/:id", validateTokenAuth, schemaValidator(blogSchema), blogController.postBlog);
 blogRouter.delete("/delete/blogs/:id", validateTokenAuth, blogController.postBlog);
 
 export default blogRouter;
